7f540ee58fcbbe79908bd65c68a6e5b9a55f97a0
Cisino Junior <cisinojr@gmail.com>
2020-01-19 13:41:45 -0300

    chore: add useCreateIndex: true to mongoose configuration

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ const app = express();
 const uri = "mongodb+srv://omnistack:omnistack@cluster0-1u44w.mongodb.net/week10?retryWrites=true&w=majority";
 const port = process.env.PORT | 3333;
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 app.use(express.json());
 app.use(routes);
 app.listen(port);
 
-console.log(`Server is running at port: ${port}`)
+console.log(`Server is running at port: ${port}`);
