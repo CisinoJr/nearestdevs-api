1b3f91f6d57731120c114d193a834bfe2e8ed5b9
Cisino Junior <cisinojr@gmail.com>
2020-01-19 13:25:26 -0300

    chore: jest, babel, express and database config

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import routes from './routes/routes';
+const mongoose = require('mongoose');
+const app = express();
+
+const uri = "mongodb+srv://omnistack:omnistack@cluster0-1u44w.mongodb.net/week10?retryWrites=true&w=majority";
+const port = process.env.PORT | 3333;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.json());
+app.use(routes);
+app.listen(port);
+
+console.log(`Server is running at port: ${port}`)
