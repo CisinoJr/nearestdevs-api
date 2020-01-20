bfd576a9b3ae8c2a61c00f8722c680da2964a68d
Cisino Junior <cisinojr@gmail.com>
2020-01-20 12:55:19 -0300

    fix: corrects port binding

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 const uri = "mongodb+srv://omnistack:omnistack@cluster0-1u44w.mongodb.net/week10?retryWrites=true&w=majority";
-const port = process.env.PORT | 3333;
+const port = process.env.PORT || 3333;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
@@ -16,4 +16,4 @@ app.use(express.json());
 app.use(routes);
 app.listen(port);
 
-console.log(`Server is running at port: ${port}`);
+console.log(`Server is running on port: ${port}`);
